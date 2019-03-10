0836514ef4b743d5875c5a88ba4097229ab3c942
brayansi <v.brayansantos@gmail.com>
2019-03-10 18:31:26 -0300

    Implementação do formulário

--- src/views/conatactView/ContactView.js
+++ src/views/conatactView/ContactView.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 import Navbar from '../components/navbar/Navbar';
+import { Row, Col, CardPanel, Input, Button } from 'react-materialize';
+import './ContactView.css';
 
 class ContactView extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
+    render() {
         return (
             <div>
                 <Navbar name="Contact" />
+                <Row>
+                    <Col l={6} m={12}>
+                        <CardPanel>
+                            <Row>
+                                <Input s={6} label="First Name" validate></Input>
+                                <Input s={6} label="Last Name" validate type='tel'></Input>
+                                <Input s={12} type="email" label="Email Address" />
+                                {/* colocar um contador de caracteres */}
+                                <Input s={12} label="Your Comments" type='textarea' />
+                                <Button waves='light' flat={true} className="btn-submit">Submit</Button>
+                            </Row>
+                        </CardPanel>
+                    </Col>
+                </Row>
             </div>
-        );    }
+        );
+    }
 }
- 
+
 export default ContactView;
